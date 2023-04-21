f8240f6dd673e63df734400c30801da31a7f6783
barwnik <jacek_stopa@hotmail.com>
2023-04-21 17:05:22 -0600

    Create Protective_Put.py
    Buying a unit of UA and also buying put as a hedge

---
+++ Protective_Put.py
@@ -0,0 +1,16 @@
+mport yfinance as yf
+
+ticker = 'AAPL'
+stock = yf.Ticker(ticker)
+stock_price = stock.info['regularMarketPrice']
+
+# Buy the stock
+def buy_stock(quantity):
+    return quantity * stock_price
+
+owned_stock = buy_stock(100)
+
+# Buy the put option
+cost = buy_put(1)
+
+print(f"Protective Put: Total cost for 1 put contract is ${cost}, and owned stock value is ${owned_stock}")
