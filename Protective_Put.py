8a3e78a88a042733f51a2603ee787d5c236fef7f
barwnik <jacek_stopa@hotmail.com>
2023-04-21 17:20:55 -0600

    Update Protective_Put.py

--- Protective_Put.py
+++ Protective_Put.py
@@ -1,16 +1,27 @@
 import yfinance as yf
 
 ticker = 'AAPL'
-stock = yf.Ticker(ticker)
-stock_price = stock.info['lastPrice']
+put_option_symbol = 'AAPL220616P00150000'
+
+# Download the put option data
+option_data = yf.Ticker(put_option_symbol).info
+
+# Check the option's price
+option_price = option_data['lastPrice']
+
+# Buy the option
+def buy_put(quantity):
+    total_cost = quantity * option_price
+    return total_cost
+
+cost = buy_put(1)
 
 # Buy the stock
 def buy_stock(quantity):
+    stock = yf.Ticker(ticker)
+    stock_price = stock.info['regularMarketPrice']
     return quantity * stock_price
 
 owned_stock = buy_stock(100)
 
-# Buy the put option
-cost = buy_put(1)
-
-print(f"Protective Put: Total cost for 1 put contract is ${cost}, and owned stock value is ${owned_stock}")
+print(f"Protective Put: Total cost for 1 put contract is ${cost:.2f}, and owned stock value is ${owned_stock:.2f}")
